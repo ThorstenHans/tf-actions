f575279c4b11836fbf92b53e2797c28e3e9f2bf4
Thorsten Hans <thorsten.hans@gmail.com>
2020-04-07 23:01:33 +0200

    feat: add application insights

--- main.tf
+++ main.tf
@@ -9,3 +9,11 @@ resource "azurerm_resource_group" "rg_demo" {
     responsible = "Thorsten Hans"
   }
 }
+
+resource "azurerm_application_insights" "ai_demo" {
+  name                = "ai_demo"
+  resource_group_name = azurerm_resource_group.rg_demo.name
+  location            = azurerm_resource_group.rg_demo.location
+  tags                = azurerm_resource_group.rg_demo.tags
+  application_type    = "web"
+}
